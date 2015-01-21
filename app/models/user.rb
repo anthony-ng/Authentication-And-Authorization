ddff0362827508cec975c2760043b8eced3faa18
anthony-ng <thomsonng123@gmail.com>
2015-01-20 21:33:39 -0800

    created bcrypted password_hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
   #TODO : Use bcrypt to store hashed passwords and authenticate users
+  def password
+    @password_hash ||= BCrypt::Password..new(password_hash)
+  end
+
+
 end
